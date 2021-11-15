b7805f29ccdf97f105f2c7bb82b15d19d77994e4
Ennuilysis <78125306+Ennuilysis@users.noreply.github.com>
2021-11-14 20:33:49 -0800

    Update random_ai.py

--- random_ai.py
+++ random_ai.py
@@ -1,48 +1,46 @@
 from ConnectNGame.src.player import Player
-import abc
+from ConnectNGame.src.board import Board
+from typing import List, Union, Tuple
+import random
+
+
 class RandomAi(Player):
-    VISIBLE_CHARACTERS = [chr(i) for i in range(ord('!'), ord('~') + 1)]
-    def __init__(self, player_number: int, players):
-        self.name: str = "RandomAI "+str(player_number)
-        self.piece: str = None
+    def __init__(self, player_number: int, player_list: Tuple(str, str, str), board):  # type: ignore
+        self.name: str = "RandomAI " + str(player_number)
+        self.piece: str = self.check_piece(player_list, board)
         self.player_num: int = player_number
-        self.check_piece(players)
 
-    @abc.abstractmethod
-    def create_player(self, player_num, player_list) -> RandomAi:
-        globals()[self.name] = RandomAi(player_num, player_list)
-        return (globals()[self.name])
+    @staticmethod
+    def create_player(player_num: int, player_list: Tuple(str, str, str), board) -> Player:  # type: ignore
+        name_AI = "RandomAI " + str(player_num)
+        globals()[name_AI] = RandomAi(player_num, player_list, board)
+        return globals()[name_AI]
 
-
-    def check_piece(self, player_num: int) -> tuple[str, str]:
-        x = [t[0] for t in player_list]
-        x = [t.lower() for t in x]
+    def check_piece(self, player_list: int, board) -> str:  # type: ignore
+        VISIBLE_CHARACTERS = [chr(i) for i in range(ord('!'), ord('~') + 1)]  # type: ignore
         y = [t[1] for t in player_list]
         while True:
-            player_name = input(f"Player {player_num} enter your name: ")
-            player_name_lower = player_name.lower()
-            if len(player_name) == 0 or player_name == "_":
-                print("Your name cannot be the empty string or whitespace")
-                continue
-            elif len(player_name.replace(" ", "")) == 0:
-                print("Your name cannot be the empty string or whitespace")
-                continue
-            elif player_name_lower in x:
-                print(f'You cannot use {player_name} for your name as someone else is already using it.')
-                continue
-            piece = input(f"Player {self.player_num} enter your piece: ")
+            pos_pop = VISIBLE_CHARACTERS.index(random.choice(VISIBLE_CHARACTERS))
+            piece = VISIBLE_CHARACTERS.pop(pos_pop)
             if len(piece.replace(" ", "")) == 0:
-                print("Your piece cannot be the empty string or whitespace")
                 continue
             elif len(piece) > 1:
-                print(f'{piece} is not a single character. Your piece can only be a single character.')
                 continue
-            elif piece == self.board.blank_character:
-                print('Your piece cannot be the same as the blank character.')
+            elif piece == board.blank_character:
                 continue
             elif piece in y:
-                pos = y.index(piece)
-                print(f'You cannot use {piece} for your piece as {self.players[pos][0]} is already using it.')
-
-            self.name = player_name
-            self.piece = piece
+                continue
+            return piece
+        
+        
+    def play(self, board: Board) -> Union[str]:
+        col = board.num_columns
+        while True:
+            pos = random.choice(range(col))
+            x: List[str] = [t[pos] for t in board.contents]
+            if pos > col - 1 or pos < 0:
+                continue
+            if board.blank_character not in x:
+                continue
+            break
+        return pos
