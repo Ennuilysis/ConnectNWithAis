8b4f089bc8a871b6c5d523b1acc8148432ad56b4
Ennuilysis <78125306+Ennuilysis@users.noreply.github.com>
2021-11-14 18:52:57 -0800

    Update simple_ai.py

--- simple_ai.py
+++ simple_ai.py
@@ -6,7 +6,7 @@ from typing import List
 class SimpleAI(RandomAi):
     #inherits the init and check piece definition from RandomAI
     @staticmethod
-    def create_player(player_num:int, player_list: List[] ) -> SimpleAI:
+    def create_player(self, player_num, player_list,board) -> SimpleAI:
         name_AI="SimpleAI "+str(player_num)
         globals()[name_AI] = SimpleAI(player_num, player_list)
         return (globals()[name_AI])
