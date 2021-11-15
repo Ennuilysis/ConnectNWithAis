d50179758499d255b7951d2c24b7e68940b767ab
Ennuilysis <78125306+Ennuilysis@users.noreply.github.com>
2021-11-15 00:04:41 -0800

    Update simple_ai.py

--- simple_ai.py
+++ simple_ai.py
@@ -1,13 +1,14 @@
-import random
 from ConnectNGame.src.players.random_ai import RandomAi
-from typing import List
+from ConnectNGame.src.board import Board
+from typing import Tuple, List
+import random
 
 
 class SimpleAI(RandomAi):
-    #inherits the init and check piece definition from RandomAI
+    # inherits the init and check piece definition from RandomAI
     @staticmethod
-    def create_player(self, player_num, player_list,board) -> SimpleAI:
-        name_AI="SimpleAI "+str(player_num)
-        globals()[name_AI] = SimpleAI(player_num, player_list)
+    def create_player(player_num: int, players: List[Tuple[str, str, int]], board: Board) -> RandomAi:
+        name_AI = "SimpleAI " + str(player_num)
+        globals()[name_AI] = SimpleAI(player_num, players, board)
+        # noinspection PyRedundantParentheses
         return (globals()[name_AI])
-    
