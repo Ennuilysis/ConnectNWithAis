0e3d833cbbeeed9c615521747dc826575873330c
Ennuilysis <78125306+Ennuilysis@users.noreply.github.com>
2021-11-14 18:54:51 -0800

    Update simple_ai.py

--- simple_ai.py
+++ simple_ai.py
@@ -11,18 +11,3 @@ class SimpleAI(RandomAi):
         globals()[name_AI] = SimpleAI(player_num, player_list)
         return (globals()[name_AI])
     
-    def check_piece(self, players: int,board) -> str:
-        VISIBLE_CHARACTERS = [chr(i) for i in range(ord('!'), ord('~') + 1)]
-        y = [t[1] for t in players]
-        while True:
-            pos_pop = VISIBLE_CHARACTERS.index(random.choice(VISIBLE_CHARACTERS))
-            piece = VISIBLE_CHARACTERS.pop(pos_pop)
-            if len(piece.replace(" ", "")) == 0:
-                continue
-            elif len(piece) > 1:
-                continue
-            elif piece == board.blank_character:
-                continue
-            elif piece in y:
-                continue
-            return piece
