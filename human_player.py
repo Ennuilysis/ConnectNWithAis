87a6f24577ba0b114969cbb1bd90c2a0da0edcbc
Ennuilysis <78125306+Ennuilysis@users.noreply.github.com>
2021-11-16 18:05:32 -0800

    Update human_player.py

--- human_player.py
+++ human_player.py
@@ -1,14 +1,13 @@
 import abc
 from ConnectNGame.src.player import Player
 from ConnectNGame.src.board import Board
-
 from typing import List, Tuple
 
 
 class HumanPlayer(Player):
     # creates human player instances
 
-     def __init__(self, player_number: int, players, board: Board):
+    def __init__(self, player_number: int, players: List[Tuple[str, str, int]], board: Board):
         self.name = None
         self.piece = None
         self.player_num = player_number
@@ -16,9 +15,9 @@ class HumanPlayer(Player):
         return
 
     @staticmethod
-    def create_player(player_num, players, board) -> Player:
-        globals()["HumanPlayer "+str(player_num)] = HumanPlayer(player_num, players, board)
-        return (globals()["HumanPlayer "+str(player_num)])
+    def create_player(player_num: int, players: List[Tuple[str, str, int]], board: Board) -> Player:
+        globals()["HumanPlayer " + str(player_num)] = HumanPlayer(player_num, players, board)
+        return (globals()["HumanPlayer " + str(player_num)])
 
     @abc.abstractmethod
     def check_name_and_piece(self, player_num: int, players, board: Board) -> None:
@@ -26,19 +25,20 @@ class HumanPlayer(Player):
         x = [t.lower() for t in x]
         y = [t[1] for t in players]
         while True:
-            player_name = input(f"Player {player_num} enter your name: ")
+            player_name = input(f"HumanPlayer {player_num} enter your name: ")
+            player_name = player_name.replace(" ", "")
+            # print(f"Player_name {player_name}")
             player_name_lower = player_name.lower()
-            if len(player_name) == 0 or player_name == "_":
-                print("Your name cannot be the empty string or whitespace")
-                continue
-            elif len(player_name.replace(" ", "")) == 0:
+            if len(player_name) == 0:
                 print("Your name cannot be the empty string or whitespace")
                 continue
             elif player_name_lower in x:
                 print(f'You cannot use {player_name} for your name as someone else is already using it.')
                 continue
-            piece = input(f"Player {self.player_num} enter your piece: ")
-            if len(piece.replace(" ", "")) == 0:
+
+            piece = input(f"HumanPlayer {self.player_num} enter your piece: ")
+            piece = piece.replace(" ", "")
+            if len(piece) == 0:
                 print("Your piece cannot be the empty string or whitespace")
                 continue
             elif len(piece) > 1:
@@ -61,10 +61,10 @@ class HumanPlayer(Player):
 
         while True:
             try:
-                pos = input(f"{self.name}, please enter the column you want to play in: ")
-                pos = int(pos)
+                pos1 = input(f"{self.name}, please enter the column you want to play in: ")
+                pos = int(pos1)
             except:
-                print(f'{player}, column needs to be an integer. {pos} is not an integer. ')  # type: ignore
+                print(f'{self.name}, column needs to be an integer. {pos1} is not an integer. ')  # type: ignore
                 continue
             if pos > col - 1 or pos < 0:
                 print(f'Your column needs to be between 0 and {col - 1} but is actually {pos}.')  # type: ignore
