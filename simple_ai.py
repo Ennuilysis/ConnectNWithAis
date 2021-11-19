30bd6ba8b982b73dc6af1286cd0d0ebe9945f315
Ennuilysis <78125306+Ennuilysis@users.noreply.github.com>
2021-11-19 13:17:34 -0800

    Update simple_ai.py

--- simple_ai.py
+++ simple_ai.py
@@ -1,7 +1,4 @@
-import random, abc
-from abc import ABC
-
-from ConnectNGame.src.players import player
+from ConnectNGame.src.player import Player
 from ConnectNGame.src.players.random_ai import RandomAi
 from ConnectNGame.src.board import Board
 from typing import Tuple, List
@@ -12,22 +9,14 @@ class SimpleAI(RandomAi):
     def __init__(self, player_num: int, players: List[Tuple[str, str, str]], board: Board):  # type: ignore
         super().__init__(player_num, players, board)
         self.name: str = "SimpleAi " + str(player_num)
-        self.piece: str = self.check_piece(players, board)
-        self.player_num: int = player_num
-    # inherits the init and check piece definition from RandomAI
+
     @staticmethod
-    def create_player(player_num: int, players, board: Board) -> RandomAi:
+    def create_player(player_num: int, players: List[Tuple[str, str, str]], board: Board, Test: bool = False) -> RandomAi:
         name = "SimpleAI " + str(player_num)
         globals()[name] = SimpleAI(player_num, players, board)
-        # noinspection PyRedundantParentheses
         return (globals()[name])
-#Where is the list of all players updated?
 
-    # def my_fun(self):
-    #     self.check_piece(...)
-# chooses the piece the same way as Random Ai
-
-    def win_check(self, piece, board) -> bool:
+    def win_check(self, piece, board) -> Tuple[int]:
         board_list: List[List[str]] = board.contents
         longest_vect = 0
 
@@ -37,10 +26,8 @@ class SimpleAI(RandomAi):
             try:
                 next_chr: str = board_list[row][col]
             except IndexError:
-                # print("problem")
                 return 0
             if next_chr == piece:
-                # print("{}", row, col, next_chr)
                 return 1 + check_next(row + row_chng, col + col_chng, row_chng, col_chng)
             else:
                 return 0
@@ -48,97 +35,36 @@ class SimpleAI(RandomAi):
         for col in range(board.num_columns):
             for row in range(board.num_rows):
                 if piece == board.contents[row][col]:
-                    # print("{}", row, col)
                     up_vect: int = check_next(row, col, -1, 0)
-                    # print("topright")
                     top_right_vect: int = check_next(row, col, 1, 1)
                     right_vect: int = check_next(row, col, 0, 1)
                     bottom_right_vect: int = check_next(row, col, -1, 1)
-                    # print("!!!!",up_vect,top_right_vect,right_vect,bottom_right_vect)
-                    longest_vect: int = max(up_vect, top_right_vect, right_vect, bottom_right_vect,
-                                            longest_vect)
-                    # print("?????",longest_vect)
+                    longest_vect: int = max(up_vect, top_right_vect, right_vect, bottom_right_vect, longest_vect)
         if longest_vect >= board.pieces_to_win:
             return True
+        else:
+            False
 
-    # chooses the piece the same way as Random Ai
-    def play(self, board):
+    def play(self, board: Board, Test: bool = False):
         col = board.num_columns
         for column in range(col):
-            board.drop_piece_into_column(column, self.piece)
+            cord = board.drop_piece_into_column(column, self.piece, cheat=True)
             win = self.win_check(self.piece, board)
-            board.take_out_piece(column)
-            # take piece back out
+            correct_row_cord=board.num_rows-cord[0]-1
+            board.fill_spot(correct_row_cord, cord[1], board.blank_character)
             if win:
                 return column
         for column in range(board.num_columns):
-            board.drop_piece_into_column(column, opponent.piece)
-            win = opponent.win_check(opponent.piece, board)
-            board.take_out_piece(column)
+            cord = board.drop_piece_into_column(column, self.piece, cheat=True)
+            correct_row_cord = board.num_rows - cord[0] - 1
+            win = self.win_check(self.piece, board)
+            board.fill_spot( correct_row_cord, cord[1], board.blank_character)
             if win:
                 return column
         while True:
             pos = random.choice(range(col))
             x: List[str] = [t[pos] for t in board.contents]
-            if pos > col - 1 or pos < 0:
-                continue
             if board.blank_character not in x:
                 continue
             break
         return pos
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # We want to iterate through the columns in contents
-        # if there is a spot to win the simple ai will play there.
-        # board.contents
-
-        # otherwise if there is a spot to block the opponent from winning the ai will play there
-
-
-        # if the top two conditions aren't met then the AI will play at the spot the same way that random AI does
-
-
-
-
-
-#bob = SimpleAI()
-#bob.check_piece()
-
-# if there is a spot to win the simple ai will play there.
-# otherwise if there is a spot to block the opponent from winning the ai will play there
-# if the top two conditions aren't met then the AI will play at the spot the same way that random AI does
-#
-
-
-
-
-
-
-
-
-#bob = SimpleAI()
-#bob.check_piece()
-
-# if there is a spot to win the simple ai will play there.
-# otherwise if there is a spot to block the opponent from winning the ai will play there
-# if the top two conditions aren't met then the AI will play at the spot the same way that random AI does
-#
-
-
-
